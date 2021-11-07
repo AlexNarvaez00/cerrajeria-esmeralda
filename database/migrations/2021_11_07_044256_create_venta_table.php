8517749e15870a729113fbde6d717421d7dec8e2
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-06 23:42:39 -0600

    migraciones omar

---
+++ database/migrations/2021_11_07_044256_create_venta_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venta', function (Blueprint $table) {
+            $table->string('folio_v',7);
+            $table->dateTime('fechayhora', $precision = 0);
+            $table->string('idusuario',7);
+            $table->string('idcliente',7);
+            $table->primary('folio_v');
+            $table->foreign('idusuario')->references('idusuario')-on('usuarios');
+            $table->foreign('idcliente')->references('idcliente')-on('cliente');           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venta');
+    }
+}
