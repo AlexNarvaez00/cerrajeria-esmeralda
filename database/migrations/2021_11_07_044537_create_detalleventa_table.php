b38bf661a29466b214b431c252e44ae5dd16bb97
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-06 23:48:45 -0600

    correcione migraciones 2

---
+++ database/migrations/2021_11_07_044537_create_detalleventa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetalleVentaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalleventa', function (Blueprint $table) {
+            $table->text('observaciones');
+            $table->integer('cantidad');
+            $table->string('idproductos',10);
+            $table->string('folio_v',7);
+            $table->foreign('idproductos')->references('idproductos')-on('productos');
+            $table->foreign('folio_v')->references('folio_v')-on('venta'); 
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalleventa');
+    }
+}
