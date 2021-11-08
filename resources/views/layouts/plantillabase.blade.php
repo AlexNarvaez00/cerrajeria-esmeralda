53ae99a9147c890f9e7b7eda078adf428dc939dd
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-07 21:32:44 -0600

    icono en la barra de titulo

--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -11,7 +11,7 @@
     <title>Cerrajeria Profesional Esmeralda</title>
   </head>
   <body>
-    <h1 class="text-black"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+    <h1 class="text-black"><svg xmlns="http://www.w3.org/2000/svg" width="55" height="49" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
   <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
 </svg>Cerrajeria Profesional Esmeralda</h1>
     <div class="conteiner">
