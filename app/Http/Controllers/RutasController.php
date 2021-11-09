d781307ebcf0cd1cb8efe3c8e932f26043b797cb
Alexis Narvaez Ruiz <zaizealexis@gmail.com>
2021-11-09 08:00:55 -0600

    rutas agregadas

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 /**
+ * Controlador para las rutas.
  * 
- * 
-*/
+ */
 class RutasController extends Controller
 {
-    
+    public function showView($nombreVista)
+    {
+        
+        return view($nombreVista);
+        // switch ($nombreVista) {
+        //     case 'usuarios':
+        //         # code...
+        //         break;
+        //     case 'menu':
+        //         break;
+
+        //         case 'pro':
+        //             break;    
+
+        //     default:
+        //         # code...
+        //         break;
+        // }
+    }
 }
