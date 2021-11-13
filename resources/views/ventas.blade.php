1adeacea30cca4b0310735f4447f7c67cfbd02f1
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-13 14:38:54 -0600

    Termine la interfaz ventas como el prototipo

--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -1,6 +1,6 @@
 @extends('rootview')
 @section('itemsmenu')
-<!--Items de la barra del menu-->
+<!--Items de la barra del menu-->    
     <li class="nav-item">
     <a class="nav-link text-dark" href="#">Productos</a>
     </li>
@@ -14,28 +14,245 @@
     <a class="nav-link text-dark" href="#">Usuarios</a>
     </li>
     <li class="nav-item">
-    <a class="nav-link text-dark" href="#">&#128276;    Notificaciones</a>
+    <a class="nav-link text-dark" href="#">&#128276; Notificaciones</a>
     </li>
+
 @endsection
 
 @section('contenido')
-<div class="row">   
-    <div class="col-2"></div>
-    <div class="col-3"> &#128075; ¡Hola, XXXX XXXX XXXX!</div>    
-  </div>
 <div class="row">
-</div>
+    <div class="col-2"></div>
+    <div class="col-3"> &#128075; ¡Hola, XXXX XXXX XXXX!</div>   
+  </div>
+
 <br>
+
 <div class="row">
     <div class="col-2"></div>
     <div class="col-2"><b>Ventas</b></div>     
 </div>
+
 <br>
-<!--
+
 <div class="row">
-    <div class="col-3 border"> 3 </div>
-    <div class="col-3 border"> 3 </div>
-    <div class="col-3 border"> 3 </div>  
-    <div class="col-3 border"> 3 </div>  
-</div>-->
+<div class="col-5"></div>   
+<div class="col-3">
+    <input type="text" class="form-control" placeholder="PlaceHolder">
+</div>
+<button type="button" class="btn btn-light col-1">&#128269; buscar</button>
+<button type="button" class="btn btn-light col-1">&#9547; Agregar</button>   
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-2"></div> 
+    <div class="col-8">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Campo 1</th>
+                    <th scope="col">Campo 2</th>
+                    <th scope="col">Campo 3</th>
+                    <th scope="col">Campo 4</th>
+                    <th scope="col">Campo 5</th>
+                    <th scope="col">Campo 6</th>
+                    <th scope="col">Editado</th>
+                    <th scope="col">Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            <tr>
+                <th scope="row">asdcsdc</th>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>asdcsdc</td>
+                <td>&#128394;</td>
+                <td>&#10060;</td>                
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 @endsection
