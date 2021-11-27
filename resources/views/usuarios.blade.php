b7129af0ed163804ebd6aa58ed665f461c1c10f1
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-27 13:49:59 -0600

    Interfaz acerca-de terminado

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -67,15 +67,13 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Campo 1</th>
-                    <th scope="col">Campo 2</th>
-                    <th scope="col">Campo 3</th>
-                    <th scope="col">Campo 4</th>
-                    <th scope="col">Campo 5</th>
-                    <th scope="col">Campo 6</th>
-                    <th scope="col">Editado</th>
-                    <th scope="col">Eliminar</th>
+                    <!--
+                        Campos de la tabla 
+                        Estos lo pienso mandar desde el controlador
+                    -->
+                    @foreach ($camposVista as $campo)
+                        <th scope="col">{{$campo}}</th>
+                    @endforeach
                 </tr>
             </thead>
             <tbody>
@@ -221,7 +219,7 @@
     @slot('tituloModal','Registrar un nuevo usuario.')
     @slot('cuerpoModal')
         <p class="px-3">
-            Formulario para registrar a un nuevo usuario.
+            Informacion basica del usuario.
         </p>
         <div class="container-fluid">
             <div class="row">
@@ -229,23 +227,46 @@
                 <div class="col-md-6 col-sm-12">
                     <div class="input-group mb-3 ">
                         <span class="input-group-text" id="basic-addon1">Id de Usuario</span>
-                        <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="inputIDUsuario">
                     </div>
                 </div>
                 <!--Columnas :v-->
                 <div class="col-md-6 col-sm-12">
                     <div class="input-group mb-3 ">
                         <span class="input-group-text" id="basic-addon1">Nombre de Usuario</span>
-                        <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="inputNombreUsuario">
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="input-group mb-3 col-md-12 col-sm-12">
                     <span class="input-group-text" id="basic-addon1">Contraseña</span>
-                    <input type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                    <input type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="inputPasswordUsuario">
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-group mb-3 col-md-12 col-sm-12">
+                    <span class="input-group-text" id="basic-addon1">Confirmar Contraseña</span>
+                    <input type="password" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" id="inputPasswordUsuarioCon">
                 </div>
             </div> 
+        </div>
+        <p class="px-3">
+            Rol del usuario.
+        </p>
+        <div class="container-fluid">
+            <div class="row">
+                <!--Columnas :v-->
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="inputRolUsuario">Options</label>
+                    <select class="form-select" id="inputRolUsuario">
+                        <option selected>Rol de usuario...</option>
+                        <option value="Trabajador">Trabajador</option>
+                        <option value="Encargado">Encargado</option>
+                        <option value="Servicio extra">Servicio extra</option>
+                    </select>
+                </div>
+            </div>
         </div>
     @endslot
     @slot('footerModal')
@@ -259,4 +280,53 @@
         </button>
     @endslot
     @endcomponent
+@endsection
+
+
+<!--En esta seccion van los scripts para cada una de las vistas-->
+@section('scritps')
+    <!--
+        En esta parte, van los scripts, pero van en una carpeta aparte,
+        esa carpeta la iba a crear Omar, el chiste es que no se escriba Codigo de 
+        Javascript con el de PHP, sino que se coloque en otro archivo y que lo cargue al
+        ultimo. 
+    -->
+    <script >
+        //Esto es un objeto, bueno, una manera de hacerlos
+        const expresionesRegulares = {
+            idUsuario: /^USU-[0-9]{3}-[A-Z]{3}$/, //Esto puede cambiar
+            nombreUsuario:/^[A-Z][a-z]{2,25}$/, //Los nombres solo pueden iniciar con mayusculas.
+            password: /^[A-Za-z0-9\_]{8,20}$/ //Contraseñas
+
+            //(([A-Z]+[a-z]+[0-9]+)|([A-Z]*[a-z]*[0-9]*))+
+        };
+
+        //Optenemos los input del formulario
+        const inputIDusuario = document.getElementById('inputIDUsuario');
+        const inputNombreUsuario = document.getElementById('inputNombreUsuario');
+        const inputPasswordUsuario = document.getElementById('inputPasswordUsuario');
+        const inputPasswordUsuarioCon = document.getElementById('inputPasswordUsuarioCon');
+
+        //Definimos la funcion que evaluara la expresion regular.
+        function evaluar(element,expresion){
+            let cadena = element.target.value;//Optenemos el valor del input
+            if(expresion.test(cadena)){
+                //Si la expresion coincide, se pone en verde
+                element.target.classList.add('is-valid') 
+                element.target.classList.remove('is-invalid')  
+            }else{
+                //Agregamos una lista al input para que se ponga en rojo
+                element.target.classList.add('is-invalid')
+                element.target.classList.remove('is-valid')    
+            }      
+        }
+
+        //Agregamos el vento escuchador "cuando una tecla se levanta"
+        inputIDusuario.addEventListener('keyup',e => evaluar(e,expresionesRegulares.idUsuario));
+        inputNombreUsuario.addEventListener('keyup',e => evaluar(e,expresionesRegulares.nombreUsuario));
+        inputPasswordUsuario.addEventListener('keyup',e => evaluar(e,expresionesRegulares.password));
+        inputPasswordUsuarioCon.addEventListener('keyup',e => evaluar(e,expresionesRegulares.password));
+
+
+    </script>
 @endsection
