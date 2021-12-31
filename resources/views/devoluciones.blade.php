18fa0cde20c4d44cccffb2f1c16814bf367edcb9
DAFNS <18161252@itoaxaca.edu.mx>
2021-12-31 11:21:14 -0600

    cambios 1

--- resources/views/devoluciones.blade.php
+++ resources/views/devoluciones.blade.php
@@ -16,12 +16,12 @@
 
 @section('contenido')
 
-<div class="col-md-6 col-sm-12">
-                    <div class="input-group mb-3 ">
-                        <span class="input-group-text" id="basic-addon1">Nombre de producto</span>
-                        <input id ="inNomProducto" maxlength="20" type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="nombre_producto" required>
-                    </div>
-                </div>
-            </div>
+    <div class="col-md-6 col-sm-12">
+        <div class="input-group mb-3 ">
+            <span class="input-group-text" id="basic-addon1">Nombre de producto</span>
+            <input id ="inNomProducto" maxlength="20" type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" name="nombre_producto" required>
+        </div>
+    </div>
+    
 
 @endsection
