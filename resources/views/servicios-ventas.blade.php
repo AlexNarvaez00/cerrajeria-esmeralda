bc99cc691de7b5f615c58655b194df3b33b24ebb
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2022-01-30 20:17:15 -0600

    Cambios

--- resources/views/servicios-ventas.blade.php
+++ resources/views/servicios-ventas.blade.php
@@ -259,11 +259,7 @@
           <h5 class="text-danger" for="basic-url">*</h5>
         </div>    
       </div>
-    </div>
-    
-
-   
-       
+    </div> 
     @endslot
     @slot('footerModal')
     <x-button-normal-form type="reset" estiloBoton="btn-outline-danger" texto="Cancelar"/>    
@@ -301,7 +297,6 @@
         </div> 
         </div>    
     </div>
-    
     @endslot
     @slot('footerModal')
       <x-button-normal-form type="reset" estiloBoton="btn-outline-danger" texto="Cerrar" data-bs-dismiss="modal"/>    
@@ -411,6 +406,24 @@
       <x-button-normal-form type="reset" estiloBoton="btn-outline-danger" texto="Cerrar" data-bs-dismiss="modal"/>   
     @endslot
     @endcomponent
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Confirmar venta servicio</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
 @section('scritps')
 <script src="./js/jquery-3.6.0.min.js"></script>
